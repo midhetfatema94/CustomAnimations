76af3227d35a5fdae6cc2973571aa70afcdba33c
Midhet Sulemani <midhetfatema94@gmail.com>
2019-10-02 18:15:21 +0300

    working on pager

--- CustomAnimations/TodoListViewController.swift
+++ CustomAnimations/TodoListViewController.swift
@@ -12,6 +12,10 @@ class TodoListViewController: UIViewController {
     
     var welcomeLabel: UILabel!
     var nameLabel: UILabel!
+    var colorArray = [UIColor.red, UIColor.blue, UIColor.green]
+    
+    @IBOutlet weak var pagingScroll: UIScrollView!
+    @IBOutlet var todoCategory: [UILabel]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +26,17 @@ class TodoListViewController: UIViewController {
         nameLabel = UILabel(frame: CGRect(x: 20, y: 95, width: 100, height: 25))
         nameLabel.text = "Midhet"
         self.view.addSubview(nameLabel)
+        
+        pagingScroll.delegate = self
+        pagingScroll.isPagingEnabled = true
+        pagingScroll.contentSize = CGSize(width: pagingScroll.bounds.width * 3, height: pagingScroll.bounds.height)
+        pagingScroll.backgroundColor = colorArray.first
     }
 }
 
+extension TodoListViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        scrollView.backgroundColor = colorArray[Int(pageIndex)]
+    }
+}
